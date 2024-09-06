7d3183b7dd26c7fcb4faa68424ac6a431b27582f
Ethan <80540543+eanderso2@users.noreply.github.com>
2024-09-06 11:06:34 -0400

    got robot working(up, down, left, right, turn)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode; import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.mechanisms.MecanumDrive;
+@TeleOp()
+public class teleop extends OpMode {
+    MecanumDrive drive = new MecanumDrive();
+    @Override
+    public void init() {
+        drive.init(hardwareMap); }
+    @Override
+    public void loop() {
+        double forward = -gamepad1.left_stick_y/3; double right = gamepad1.left_stick_x/3; double rotate = gamepad1.right_stick_x/3;
+        drive.drive(forward, right, rotate); }
+}
