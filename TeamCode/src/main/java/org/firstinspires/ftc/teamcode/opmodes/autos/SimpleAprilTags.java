673230758fe3da5a8b8a2e1bc9792bd5bd06a276
Ethan <80540543+eanderso2@users.noreply.github.com>
2024-09-09 07:45:59 -0400

    added april tag IDing and collection of robot current position on the field

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/SimpleAprilTags.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/SimpleAprilTags.java
@@ -1,32 +1,30 @@
-package org.firstinspires.ftc.teamcode.opmodes;
+package org.firstinspires.ftc.teamcode.opmodes.autos;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
-import org.firstinspires.ftc.teamcode.mechanisms.MecanumDrive;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagPoseFtc;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
+import java.util.AbstractList;
+import java.util.ArrayList;
 import java.util.List;
 
-@TeleOp()
-public class teleop extends OpMode {
+@Autonomous()
+public class SimpleAprilTags extends OpMode {
     private AprilTagProcessor aprilTagProcessor;
     private VisionPortal visionPortal;
 
-    MecanumDrive drive = new MecanumDrive();
-
     @Override
     public void init() {
-        drive.init(hardwareMap);
-
         WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
         aprilTagProcessor = AprilTagProcessor.easyCreateWithDefaults();
         visionPortal = VisionPortal.easyCreateWithDefaults(webcamName, aprilTagProcessor);
     }
-
+    @Override
     public void init_loop() {
         List<AprilTagDetection> currentDetections = aprilTagProcessor.getDetections();
         StringBuilder idsFound = new StringBuilder();
@@ -41,16 +39,9 @@ public class teleop extends OpMode {
             telemetry.addData("Robot Position from April Tags", pos.getPosition());
         }
     }
-
+    @Override
     public void start() {
         visionPortal.stopStreaming(); }
-
     @Override
-    public void loop() {
-        double forward = -gamepad1.left_stick_y/3;
-        double right = gamepad1.left_stick_x/3;
-        double rotate = gamepad1.right_stick_x/3;
-
-        drive.drive(forward, right, rotate);
-    }
+    public void loop() { }
 }
