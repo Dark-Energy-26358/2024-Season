edcf04bc85714a8c1a4a43e3e8b8e9b6909c4cd3
Ethan <80540543+eanderso2@users.noreply.github.com>
2024-09-13 13:53:46 -0400

    added camera class witch alows you to get the position of the robot from april tags

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop.java
@@ -3,13 +3,9 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.teamcode.mechanisms.MecanumDrive;
 import org.firstinspires.ftc.vision.VisionPortal;
-import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
-
-import java.util.List;
 
 @TeleOp()
 public class teleop extends OpMode {
@@ -27,30 +23,14 @@ public class teleop extends OpMode {
         visionPortal = VisionPortal.easyCreateWithDefaults(webcamName, aprilTagProcessor);
     }
 
-    public void init_loop() {
-        List<AprilTagDetection> currentDetections = aprilTagProcessor.getDetections();
-        StringBuilder idsFound = new StringBuilder();
-        Pose3D pos = null;
-        for (AprilTagDetection detection : currentDetections) { idsFound.append(detection.id);
-            pos = detection.robotPose;
-            idsFound.append(' ');
-
-        }
-        telemetry.addData("April Tags", idsFound);
-        if(pos != null) {
-            telemetry.addData("Robot Position from April Tags", pos.getPosition());
-        }
-    }
-
-    public void start() {
-        visionPortal.stopStreaming(); }
 
     @Override
     public void loop() {
-        double forward = -gamepad1.left_stick_y/3;
-        double right = gamepad1.left_stick_x/3;
-        double rotate = gamepad1.right_stick_x/3;
+        double forward = -gamepad1.left_stick_y/10;
+        double right = gamepad1.left_stick_x/10;
+        double rotate = gamepad1.right_stick_x/10;
 
         drive.drive(forward, right, rotate);
+        telemetry.addData("robot rotation", drive.rotation);
     }
 }
