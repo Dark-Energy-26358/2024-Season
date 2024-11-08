0645e2404b501e69b85850adc8bc6a48a000759b
MatthewWHuang <64226660+MatthewWHuang@users.noreply.github.com>
2024-11-08 13:10:38 -0500

    Added Chin Up Arm and interface for other arms

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -2,13 +2,16 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.teamcode.mechanisms.ChinUpArm;
 import org.firstinspires.ftc.teamcode.mechanisms.MecanumDrive;
 
 public class Robot {
     public MecanumDrive mecanumDrive = new MecanumDrive();
+    public ChinUpArm chinUpArm = new ChinUpArm();
 
     public void init(HardwareMap hardwareMap){
         mecanumDrive.init(hardwareMap);
+        chinUpArm.init(hardwareMap);
     }
 }
 
