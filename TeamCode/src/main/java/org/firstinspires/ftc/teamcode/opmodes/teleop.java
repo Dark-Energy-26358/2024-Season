19bed527dd65bc937a0242a4cf61a2c174b8e0e5
Tsuloni Eventide <179648679+Tsuloni@users.noreply.github.com>
2024-11-08 13:06:36 -0500

    tying out the limelight camera, but it is not working yet

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop.java
@@ -1,14 +1,28 @@
 package org.firstinspires.ftc.teamcode.opmodes;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.mechanisms.MecanumDrive;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.LLStatus;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+
 @TeleOp()
 public class teleop extends OpMode {
     MecanumDrive drive = new MecanumDrive();
+    private Limelight3A limelight;
     @Override
     public void init() {
         drive.init(hardwareMap);
+
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        telemetry.setMsTransmissionInterval(11);
+        limelight.pipelineSwitch(0);
+        //Starts polling for data.
+        limelight.start();
     }
     @Override
     public void loop() {
