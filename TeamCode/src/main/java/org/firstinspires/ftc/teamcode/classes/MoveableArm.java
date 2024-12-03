feef597b632bab96558e0840ace86f4a16e8d0ee
Ethan <80540543+eanderso2@users.noreply.github.com>
2024-12-02 20:00:26 -0500

    got setTargetArmExtension and will have setTargetArmRotation this meeting(i think)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/classes/MoveableArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/classes/MoveableArm.java
@@ -9,8 +9,8 @@ public class MoveableArm implements org.firstinspires.ftc.teamcode.Interfaces.Mo
     public int currentArmExtension = 0;
     public int targetArmExtension = 0;
 
-    public int currentArmRotation = 0;
-    public int targetArmRotation = 0;
+    public double currentArmRotation = 0;
+    public double targetArmRotation = 0;
 
     public String extensionMotorName = "";
     public String rotationMotorName = "";
@@ -18,58 +18,66 @@ public class MoveableArm implements org.firstinspires.ftc.teamcode.Interfaces.Mo
     DcMotor extensionMotor;
     DcMotor rotationMotor;
 
+    public boolean stopped;
+
     public void init(HardwareMap hardwareMap) {
         extensionMotor = hardwareMap.dcMotor.get(extensionMotorName);
         rotationMotor = hardwareMap.dcMotor.get(rotationMotorName);
 
-        extensionMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        extensionMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         rotationMotor.setDirection(DcMotorSimple.Direction.FORWARD);
 
+
         extensionMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        extensionMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
         rotationMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rotationMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        extensionMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rotationMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
-
-
-    public void moveTowardsTargetArmExtension(int targetExtension) { //TODO dod dis
-        targetArmExtension = targetExtension;
-
-        if (-extensionMotor.getCurrentPosition() < 0){
-            extensionMotor.setPower(0.01);
-        }else{
-            extensionMotor.setPower(-0.01);
-        }
-//        if (extensionMotor.getCurrentPosition() > targetArmExtension + 100) {
-//            extensionMotor.setPower(-0.1);
-//        }else {
-//            extensionMotor.setPower(0);
-//        }
+    public void stop() {
+        stopped = true;
+        extensionMotor.setPower(0);
+        rotationMotor.setPower(0);
+        extensionMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
+//27.5 in = 3200 tick(s)
+    public void setTargetArmExtension(int targetExtension) {
+        extensionMotor.setTargetPosition(targetExtension*116);
+        extensionMotor.setPower(0.5);
+        extensionMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+    //1.5˚ = 1 tick(s)
     public void setTargetArmRotation(int targetRotation) {
-        rotationMotor.setTargetPosition(targetRotation);
-        targetArmRotation = targetRotation;
+        stopped = false;
+        targetArmRotation = targetRotation*1.5;
+        extensionMotor.setPower(0.5);
+        extensionMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
 
 
     public int getTargetArmExtension() {
-        return targetArmExtension;
+        return targetArmExtension/116;
     }
 
-    public int getTargetArmRotation() {
-        return targetArmRotation;
+    public double getTargetArmRotation() {
+        return targetArmRotation/1.5;
     }
 
 
     public int getCurrentArmExtension() {
         currentArmExtension = extensionMotor.getCurrentPosition();
-        return currentArmExtension;
+        return currentArmExtension/116;
     }
 
-    public int getCurrentArmRotation() {
+    public double getCurrentArmRotation() {
         currentArmRotation = rotationMotor.getCurrentPosition();
-        return currentArmRotation;
+        return currentArmRotation/1.5;
     }
 
 
