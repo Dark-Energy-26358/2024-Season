a7b90a0729b0ec3e8a215c1f447d13266eb157db
Ethan <80540543+eanderso2@users.noreply.github.com>
2025-01-13 18:52:39 -0500

    got setTargetArmExtension and will have setTargetArmRotation this meeting(i think)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/classes/MoveableArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/classes/MoveableArm.java
@@ -13,35 +13,25 @@ public class MoveableArm implements org.firstinspires.ftc.teamcode.Interfaces.Mo
     public double targetArmRotation = 0;
 
     public String extensionMotorName = "";
-    public String rotationMotorName = "";
 
-    DcMotor extensionMotor;
-    DcMotor rotationMotor;
+    public DcMotor extensionMotor;
 
     public boolean stopped;
 
     public void init(HardwareMap hardwareMap) {
         extensionMotor = hardwareMap.dcMotor.get(extensionMotorName);
-        rotationMotor = hardwareMap.dcMotor.get(rotationMotorName);
 
         extensionMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        rotationMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-
 
         extensionMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         extensionMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        rotationMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rotationMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
         extensionMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rotationMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     public void stop() {
         stopped = true;
         extensionMotor.setPower(0);
-        rotationMotor.setPower(0);
         extensionMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
@@ -50,23 +40,21 @@ public class MoveableArm implements org.firstinspires.ftc.teamcode.Interfaces.Mo
         extensionMotor.setTargetPosition(targetExtension*116);
         extensionMotor.setPower(0.5);
         extensionMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        targetArmExtension = extensionMotor.getTargetPosition();
     }
     //1.5˚ = 1 tick(s)
     public void setTargetArmRotation(int targetRotation) {
-        stopped = false;
-        targetArmRotation = targetRotation*1.5;
-        extensionMotor.setPower(0.5);
-        extensionMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
     }
 
 
 
     public int getTargetArmExtension() {
-        return targetArmExtension/116;
+        return extensionMotor.getTargetPosition()/116;
     }
 
     public double getTargetArmRotation() {
-        return targetArmRotation/1.5;
+        return 0;
     }
 
 
@@ -76,8 +64,7 @@ public class MoveableArm implements org.firstinspires.ftc.teamcode.Interfaces.Mo
     }
 
     public double getCurrentArmRotation() {
-        currentArmRotation = rotationMotor.getCurrentPosition();
-        return currentArmRotation/1.5;
+        return 0;
     }
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/ManipulatorArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/ManipulatorArm.java
@@ -1,16 +1,55 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.classes.MoveableArm;
 
 public class ManipulatorArm extends MoveableArm {
+    public String rotationMotorName = "manipulatorRotationMotor";
+
+    DcMotor rotationMotor;
 
     @Override
     public void init(HardwareMap hardwareMap) {
         extensionMotorName = "manipulatorExtensionMotor";
-        rotationMotorName = "manipulatorRotationMotor";
+
+        rotationMotor = hardwareMap.dcMotor.get(rotationMotorName);
+
+        rotationMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        rotationMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rotationMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        rotationMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         super.init(hardwareMap);
+    }
+
+    @Override
+    public void stop() {
+        rotationMotor.setPower(0);
+        super.stop();
+    }
+
+    @Override
+    public void setTargetArmRotation(int targetRotation) {
+        stopped = false;
+        rotationMotor.setTargetPosition(targetRotation*116);
+        rotationMotor.setPower(0.5);
+        rotationMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        targetArmRotation = rotationMotor.getTargetPosition();
+    }
+
+    @Override
+    public double getTargetArmRotation() {
+        return targetArmRotation/1.5;
+    }
+
+    @Override
+    public double getCurrentArmRotation() {
+        currentArmRotation = rotationMotor.getCurrentPosition();
+        return currentArmRotation/1.5;
     }
     //TODO setTargetManipulatorWristPosition
     //TODO setTargetManipulatorElbowPosition
