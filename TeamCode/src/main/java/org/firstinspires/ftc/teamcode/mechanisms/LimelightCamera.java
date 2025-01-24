8334797f11a7f909fcf326e85a3eddfe2f671c04
Vincent Batts <vbatts@hashbangbash.com>
2025-01-24 12:13:57 -0500

    mechanisms: carry-over classes from prior branch
    from edcf04bc85714a8c1a4a43e3e8b8e9b6909c4cd3
    12f546762b93f7094145b149909c6046336bebbe
    914f68600996be83e33ddc97386a93881014dd9d
    
    by @eanderso2 and @MatthewWHuang
    
    Signed-off-by: Vincent Batts <vbatts@hashbangbash.com>

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/LimelightCamera.java
@@ -0,0 +1,64 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.LLStatus;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class LimelightCamera {
+    private Pose3D pos = null;
+    private Limelight3A limelight;
+
+    public void init(HardwareMap hardwareMap) {
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+
+        limelight.pipelineSwitch(0);
+
+        /*
+         * Starts polling for data.
+         */
+        limelight.start();
+    }// }
+
+    private void updateCamera() {
+        LLResult result = limelight.getLatestResult();
+        if (result != null) {
+            if (result.isValid()) {
+                pos = result.getBotpose();
+            }
+        }
+    }
+
+    public Position getPosition(){
+        updateCamera();
+        if(pos != null) {
+            return pos.getPosition();
+        }
+        return null;
+    }
+    public YawPitchRollAngles getOrientation(){
+        updateCamera();
+        if(pos != null) {
+            return pos.getOrientation();
+        }
+        return null;
+    }
+    public void stop(){
+        limelight.stopStreaming();
+    }
+
+}
