d142e88eaa94e5927bdb9521b02bd7cd3a9a8675
MatthewWHuang <64226660+MatthewWHuang@users.noreply.github.com>
2025-01-24 14:37:37 -0500

    Added (hypothetically) working ScorePreloadAndPark

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.teamcode.mechanisms.LimelightCamera;
 import org.firstinspires.ftc.teamcode.mechanisms.StageOneAscentArms;
 import org.firstinspires.ftc.teamcode.mechanisms.ManipulatorArm;
 import org.firstinspires.ftc.teamcode.mechanisms.StageTwoAscentArms;
@@ -14,11 +15,13 @@ public class Robot {
 //    public StageTwoAscentArms stageTwoAscentArms = new StageTwoAscentArms();
     public StageOneAscentArms stageOneAscentArms = new StageOneAscentArms();
 
+    public LimelightCamera camera = new LimelightCamera();
     public void init(HardwareMap hardwareMap){
         mecanumDrive.init(hardwareMap);
         manipulatorArm.init(hardwareMap);
         //stageTwoAscentArms.init(hardwareMap);
         stageOneAscentArms.init(hardwareMap);
+        camera.init(hardwareMap);
     }
 }
 
