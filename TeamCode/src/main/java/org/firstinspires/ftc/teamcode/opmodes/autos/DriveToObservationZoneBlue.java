83889f7dd0d3cc60ab2a1d4456168b76461beaa1
MatthewWHuang <64226660+MatthewWHuang@users.noreply.github.com>
2025-01-24 13:05:41 -0500

    Grouped OpModes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/DriveToObservationZoneBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/DriveToObservationZoneBlue.java
@@ -1,19 +1,21 @@
 package org.firstinspires.ftc.teamcode.opmodes.autos;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.Robot;
 
-public class driveToObservationZoneBlue extends OpMode {
+@Autonomous(name = "Drive to Observation Zone", group = "Blue")
+public class DriveToObservationZoneBlue extends OpMode {
+
     Robot robot = new Robot();
 
     boolean manipArmAccurate = false;
 
     @Override
     public void init() {
-        robot.init(hardwareMap );
+        robot.init(hardwareMap);
     }
 
     public void loop() {
