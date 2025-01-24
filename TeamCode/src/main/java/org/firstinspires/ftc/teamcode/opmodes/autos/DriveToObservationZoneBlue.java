cd8e2e91591b79acc97c5714ae0e14b0cadd4783
Vincent Batts <vbatts@hashbangbash.com>
2025-01-24 13:49:54 -0500

    Globals: make a singleton to store globals settings
    Signed-off-by: Vincent Batts <vbatts@hashbangbash.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/DriveToObservationZoneBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/DriveToObservationZoneBlue.java
@@ -17,17 +17,16 @@ public class DriveToObservationZoneBlue extends OpMode {
     }
 
     public void loop() {
-        // BEGIN -- THIS IS NOT IDEMPOTENT and CAN ONLY BE RUN ONCE PER MATCH
-        if (!robot.manipArmAccurate) {
+        // USING GLOBAL TO BE IDEMPOTENT
+        if (!robot.globals.getManipArmAccurate()) {
             robot.manipulatorArm.setTargetArmRotation(40);
+        } else {
+            if (robot.manipulatorArm.getCurrentArmRotation() >= 40) {
+                robot.manipulatorArm.rotationMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                robot.manipulatorArm.setTargetArmRotation(0);
+                robot.globals.setManipArmAccurate(true);
+            }
         }
-
-        if (robot.manipulatorArm.getCurrentArmRotation() >= 40 & !robot.manipArmAccurate) {
-            robot.manipulatorArm.rotationMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            robot.manipArmAccurate = true;
-            robot.manipulatorArm.setTargetArmRotation(0);
-        }
-        // END -- THIS IS NOT IDEMPOTENT and CAN ONLY BE RUN ONCE PER MATCH
 
 
 
