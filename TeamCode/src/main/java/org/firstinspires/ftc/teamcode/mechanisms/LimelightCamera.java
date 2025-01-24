03a81dbd4fa5f54e018a96f87a6ce73a0090db5e
MatthewWHuang <64226660+MatthewWHuang@users.noreply.github.com>
2025-01-24 12:19:41 -0500

    Fixed therror!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!111111

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/LimelightCamera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/LimelightCamera.java
@@ -3,20 +3,12 @@ package org.firstinspires.ftc.teamcode.mechanisms;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
-import org.firstinspires.ftc.vision.VisionPortal;
-import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
-import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
-import com.qualcomm.hardware.limelightvision.LLResult;
-import com.qualcomm.hardware.limelightvision.LLResultTypes;
-import com.qualcomm.hardware.limelightvision.LLStatus;
-import com.qualcomm.hardware.limelightvision.Limelight3A;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
 
 
 public class LimelightCamera {
@@ -58,7 +50,7 @@ public class LimelightCamera {
         return null;
     }
     public void stop(){
-        limelight.stopStreaming();
+        limelight.stop();
     }
 
 }
