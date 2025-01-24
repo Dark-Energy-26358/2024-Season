be4061bd57ecd8191855c74b8665fae0978ac3a1
MatthewWHuang <64226660+MatthewWHuang@users.noreply.github.com>
2025-01-24 13:16:54 -0500

    IM' TYPING WURDS!!!!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.Robot;
 
-@TeleOp(name="Standard")
+@TeleOp(name="Standard TeleOp")
 public class Teleop extends OpMode {
 
     Robot robot = new Robot();
