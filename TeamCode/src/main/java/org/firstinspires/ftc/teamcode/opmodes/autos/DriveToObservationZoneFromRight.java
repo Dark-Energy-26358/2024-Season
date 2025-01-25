855e0d37438af7f67e27e0a2016afe4fb3de62c4
Vincent Batts <vbatts@hashbangbash.com>
2025-01-25 14:23:42 -0500

    DriveRight: simple drive right for 30s
    Signed-off-by: Vincent Batts <vbatts@hashbangbash.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/DriveToObservationZoneFromRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/DriveToObservationZoneFromRight.java
@@ -7,15 +7,21 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import org.firstinspires.ftc.teamcode.Robot;
 
 @Autonomous(name = "Drive to Observation Zone", preselectTeleOp = "TeleOp", group = "Blue")
-public class DriveToObservationZoneBlue extends OpMode {
+public class DriveToObservationZoneFromRight extends OpMode {
 
     Robot robot = new Robot();
+    private long startTime;
 
     @Override
     public void init() {
         robot.init(hardwareMap);
     }
 
+    @Override
+    public void start() {
+        this.startTime = System.currentTimeMillis();
+        super.start();
+    }
     public void loop() {
         // USING GLOBAL TO BE IDEMPOTENT
         if (!robot.globals.getManipArmAccurate()) {
@@ -27,6 +33,9 @@ public class DriveToObservationZoneBlue extends OpMode {
             }
         }
 
+        if (getAgeInSeconds()<30) {
+            robot.mecanumDrive.drive(0, 0.2, 0);
+        }
 
         //TODO: DO DIS
         //plans:
@@ -35,5 +44,10 @@ public class DriveToObservationZoneBlue extends OpMode {
         //turn to drop piece
         //drop piece
         //go to hang zone
+    }
+
+    public int getAgeInSeconds() {
+        long nowMillis = System.currentTimeMillis();
+        return (int)((nowMillis - this.startTime) / 1000);
     }
 }
