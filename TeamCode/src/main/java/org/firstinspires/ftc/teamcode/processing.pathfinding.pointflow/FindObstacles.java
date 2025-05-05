af8a7b00779660d1b4d8c512f288d6eb63becd0e
Ethan <80540543+eanderso2@users.noreply.github.com>
2025-05-05 19:40:47 -0400

    made rectangular obsticles

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/processing.pathfinding.pointflow/FindObstacles.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/processing.pathfinding.pointflow/FindObstacles.java
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.teamcode.processing.pathfinding.pointflow;
 
 public class FindObstacles {
-    static final Obstacle[] fieldObstacles = {new Obstacle(0, 0, 48*Math.sqrt(2), 1)};
+    static final Obstacle[] fieldObstacles = {new Obstacle( 24, 24, -24, -24,1)};
 
     public static Obstacle[] findObstacles() {
         return fieldObstacles; // We can add more stuff here
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/processing.pathfinding.pointflow/Obstacle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/processing.pathfinding.pointflow/Obstacle.java
@@ -1,20 +1,22 @@
 package org.firstinspires.ftc.teamcode.processing.pathfinding.pointflow;
 
 public class Obstacle {
-    double x;
-    double y;
-    double radius;
+    double x1;
+    double y1;
+    double x2;
+    double y2;
 
     double height;
 
-    public Obstacle(double x, double y, double radius, double height) {
-        this.x = x;
-        this.y = y;
-        this.radius = radius;
+    public Obstacle(double x1, double y1,double x2, double y2, double height) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
         this.height = height;
     }
 
-    public Vector relativeVector(double robotX, double robotY) { // TODO: come from closest point on the circle
-        return new Vector(this.x - robotX, this.y - robotY);
+    public Vector relativeVector(double robotX, double robotY) {
+        return new Vector(Math.max(Math.min(Math.max(x1,x2),robotX),Math.min(x1,x2)), Math.max(Math.min(Math.max(y1,y2),robotY),Math.min(y1,y2)));
     }
 }
