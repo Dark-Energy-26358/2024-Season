421ced5d3f7456f014ec6d922dc3cff2a2ff23b5
Matthew <64226660+MatthewWHuang@users.noreply.github.com>
2025-05-05 19:49:13 -0400

    Simplified pathfinding a bit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/processing.pathfinding.pointflow/Pathfinding.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/processing.pathfinding.pointflow/Pathfinding.java
@@ -5,12 +5,12 @@ import org.firstinspires.ftc.robotcore.external.navigation.Position;
 public class Pathfinding {
     private static Vector pushAway(double xMovement, double yMovement, double robotX, double robotY) {
         Obstacle[] obstacles = FindObstacles.findObstacles();
-        Vector direction = new Vector(0, 0);
+        Vector direction = new Vector(xMovement, yMovement);
         for (Obstacle obstacle: obstacles) {
             direction = direction.add(obstacle.relativeVector(robotX, robotY).normalize(obstacle.height)); // probably change the height depending on other stuff
         }
 
-        return new Vector(Math.max(Math.min(direction.x+xMovement, 1), -1), Math.max(Math.min(direction.y+yMovement, 1), -1));
+        return new Vector(Math.max(Math.min(direction.x, 1), -1), Math.max(Math.min(direction.y, 1), -1));
     }
 
     public static Vector pathfind(Position robotPosition, double targetX, double targetY) {
