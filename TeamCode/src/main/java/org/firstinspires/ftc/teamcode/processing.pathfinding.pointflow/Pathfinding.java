9f65d428e42645afe45f2ca130b2a590ca29e5e7
Matthew <64226660+MatthewWHuang@users.noreply.github.com>
2025-05-10 14:18:56 -0400

    Constrained pathfinding direction before flow to make it not break from that

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/processing.pathfinding.pointflow/Pathfinding.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/processing.pathfinding.pointflow/Pathfinding.java
@@ -3,14 +3,18 @@ package org.firstinspires.ftc.teamcode.processing.pathfinding.pointflow;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
 
 public class Pathfinding {
+    private static double constrain(double num) {
+        return Math.max(Math.min(num, 1), -1);
+    }
+
     private static Vector pushAway(double xMovement, double yMovement, double robotX, double robotY) {
         Obstacle[] obstacles = FindObstacles.findObstacles();
-        Vector direction = new Vector(xMovement, yMovement);
+        Vector direction = new Vector(constrain(xMovement), constrain(yMovement));
         for (Obstacle obstacle: obstacles) {
             direction = direction.add(obstacle.relativeVector(robotX, robotY).normalize(obstacle.height)); // probably change the height depending on other stuff
         }
 
-        return new Vector(Math.max(Math.min(direction.x, 1), -1), Math.max(Math.min(direction.y, 1), -1));
+        return new Vector(constrain(direction.x), constrain(direction.y));
     }
 
     public static Vector pathfind(Position robotPosition, double targetX, double targetY) {
