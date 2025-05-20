b14d2e8b6b93821900096defac032afbd87fccb7
Matthew <64226660+MatthewWHuang@users.noreply.github.com>
2025-05-20 10:25:26 -0400

    Added the two foot rule

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/processing.pathfinding.pointflow/CalculateForce.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/processing.pathfinding.pointflow/CalculateForce.java
@@ -3,6 +3,9 @@ package org.firstinspires.ftc.teamcode.processing.pathfinding.pointflow;
 public class CalculateForce {
 
     public static double calculateForce(double distance, double height) {
+        if (distance >= 48) {
+            return 0;
+        }
         return (height/(distance + 1));
     }
 }
