64f0fb4ef90600c56e5d8ea75b84540b9da6e9e0
Ethan <80540543+eanderso2@users.noreply.github.com>
2025-05-27 13:16:51 -0400

    reforated obsticles

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/learningtemplateauto/AutoBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/learningtemplateauto/AutoBase.java
@@ -1,11 +1,15 @@
 package org.firstinspires.ftc.teamcode.opmodes.autos.learningtemplateauto;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "Ethan's team", preselectTeleOp = "TeleOp", group = "Ethan")
 public class AutoBase extends org.firstinspires.ftc.teamcode.opmodes.autos.AutoBase {
     final double TILE = 24;
 
     int stage = 1;
     double TARGET_X = 0.0; // CHANGE_ME!!
-    double TARGET_Y = 0.0; // CHANGE_ME!!
+    double TARGET_Y = 0.0
+            ; // CHANGE_ME!!
 
     @Override
     public void init() {
@@ -15,12 +19,23 @@ public class AutoBase extends org.firstinspires.ftc.teamcode.opmodes.autos.AutoB
     public void loop() {
         switch (stage) {
             case 1:
+                TARGET_X = -60;
+                TARGET_Y = -40;
                 if (robot.mecanumDrive.driveToPosition(TARGET_X, TARGET_Y, position, angles))
                     stage++;
                 break;
             case 2:
-                // TODO: Go somewhere else here
-                break; // Don't forget this break!
+                TARGET_X = 48;
+                TARGET_Y = -36;
+                if (robot.mecanumDrive.driveToPosition(TARGET_X, TARGET_Y, position, angles))
+                    stage++;
+                break;
+            case 3:
+                TARGET_X = 48;
+                TARGET_Y = 60;
+                if (robot.mecanumDrive.driveToPosition(TARGET_X, TARGET_Y, position, angles))
+                    stage++;
+                break;
         }
 
         super.loop(); // Don't delete this!
