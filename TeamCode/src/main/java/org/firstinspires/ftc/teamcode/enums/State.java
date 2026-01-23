3dfa927b859fd8710d2935a8d64f79a32952df5e
Matthew <64226660+MatthewWHuang@users.noreply.github.com>
2026-01-23 15:46:36 -0500

    Purple shell

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/enums/State.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/enums/State.java
@@ -3,6 +3,9 @@ package org.firstinspires.ftc.teamcode.enums;
 public enum State {
     INIT,
     INTAKE_READY,
+    INTAKE,
+    INTAKE_PROCESS,
+    SORTING_INTAKE,
     SORTING_TO_READY,
     SORTING_FOR_LAUNCH,
     LAUNCHING,
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop.java
@@ -14,9 +14,8 @@ public class Teleop extends OpMode {
     Robot robot = new Robot();
     ColorSensor sensor;
     boolean yPressed = false;
-    boolean bPressed = false;
-
     State state = State.INIT;
+    boolean intakeRunning = false;
     int stateFrames;
 
     @Override
@@ -42,6 +41,10 @@ public class Teleop extends OpMode {
             if (stateFrames == 0) {
                 stateFrames = 25;
                 switch (state) {
+                    case INTAKE_PROCESS:
+                        robot.sorter.increasePos(2);
+                        state = State.SORTING_INTAKE;
+                        break;
                     case SORTING_FOR_LAUNCH:
                         robot.shooter.shoot(1);
                         state = State.LAUNCHING;
@@ -53,6 +56,15 @@ public class Teleop extends OpMode {
                     case FINISH_LAUNCHING:
                         robot.shooter.reset();
                         state = State.INTAKE_READY;
+                        stateFrames = 0;
+                        break;
+                    case SORTING_INTAKE:
+                        if (intakeRunning) {
+                            state = State.INTAKE;
+                        } else {
+                            state = State.INTAKE_READY;
+                        }
+                        stateFrames = 0;
                         break;
                     default:
                         state = State.INTAKE_READY;
@@ -61,20 +73,39 @@ public class Teleop extends OpMode {
             }
         }
 
+        if (state.equals(State.INTAKE) && !robot.sorter.getColor(0).equals(DecodeColor.EMPTY)) {
+            state = State.INTAKE_PROCESS;
+            stateFrames = 10;
+        }
+
         // Passively sort if we have an empty slot
         if (robot.sorter.getNumberOfBalls() < 3) {
             if (stateFrames == 0 && (state.equals(State.INTAKE_READY) || state.equals(State.INIT))) {
                 // Make the intake slot empty
                 if (!robot.sorter.getColor(0).equals(DecodeColor.EMPTY)) {
                     robot.sorter.increasePos(2);
-                    state = State.SORTING_TO_READY;
+                    if (!state.equals(State.SORTING_INTAKE)) {
+                        state = State.SORTING_TO_READY;
+                    }
                     stateFrames = 25;
                 } else {
+                    if (state.equals(State.INIT)) {
+                        state = State.INTAKE_READY;
+                    }
+                }
+            }
+        } else {
+            if (intakeRunning) {
+                gamepad1.rumble(5000);
+                robot.intake.stop();
+                intakeRunning = false;
+                if (state.equals(State.INTAKE)) {
                     state = State.INTAKE_READY;
                 }
             }
         }
 
+        // Green launch
         if (gamepad1.aWasPressed()) {
             if (state.equals(State.INTAKE_READY)) {
                 if (robot.sorter.getColor(0).equals(DecodeColor.GREEN)) {
@@ -86,6 +117,27 @@ public class Teleop extends OpMode {
                     state = State.SORTING_FOR_LAUNCH;
                     stateFrames = 25;
                 } else if (robot.sorter.getColor(2).equals(DecodeColor.GREEN)) {
+                    robot.sorter.increasePos(1);
+                    state = State.SORTING_FOR_LAUNCH;
+                    stateFrames = 25;
+                } else {
+                    gamepad1.rumble(1000);
+                }
+            }
+        }
+
+        // Purple launch
+        if (gamepad1.xWasPressed()) {
+            if (state.equals(State.INTAKE_READY)) {
+                if (robot.sorter.getColor(0).equals(DecodeColor.PURPLE)) {
+                    robot.sorter.increasePos(3);
+                    state = State.SORTING_FOR_LAUNCH;
+                    stateFrames = 25;
+                } else if (robot.sorter.getColor(1).equals(DecodeColor.PURPLE)) {
+                    robot.sorter.increasePos(-1);
+                    state = State.SORTING_FOR_LAUNCH;
+                    stateFrames = 25;
+                } else if (robot.sorter.getColor(2).equals(DecodeColor.PURPLE)) {
                     robot.sorter.increasePos(1);
                     state = State.SORTING_FOR_LAUNCH;
                     stateFrames = 25;
@@ -103,12 +155,14 @@ public class Teleop extends OpMode {
             }
         }
         if (gamepad1.bWasPressed()) {
-            if (!bPressed) {
+            if (!intakeRunning) {
                 robot.intake.start();
-                bPressed = true;
+                state = State.INTAKE;
+                intakeRunning = true;
             } else {
                 robot.intake.stop();
-                bPressed = false;
+                state = State.INTAKE_READY;
+                intakeRunning = false;
             }
         }
 
